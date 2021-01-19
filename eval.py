09caa7a9000a549b41bc942495fd37a7cf5708ed
Yiyi Wang <yiyi@michaels.com>
2021-01-19 00:34:58 -0800

    update execution sequence

--- eval.py
+++ eval.py
@@ -195,6 +195,7 @@ def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, ma
         
         # TESTING
         seg_masks = copy.deepcopy(masks)
+        img_gpu = img_gpu * seg_masks.prod(dim=0)
         # seg_masks[seg_masks==1] = 2
         # seg_masks[seg_masks==0] = 1
         # seg_masks[seg_masks==2] = 0
@@ -217,9 +218,6 @@ def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, ma
             masks_color_summand += masks_color_cumul.sum(dim=0)
 
         # img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
-        # TESTING
-        img_gpu = img_gpu * seg_masks.prod(dim=0)
-        # TESTING
 
     if args.display_fps:
             # Draw the box for the fps on the GPU
