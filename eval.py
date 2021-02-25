8a4320331b0c38522f03708b2dd3b3e5d917b1c9
Yiyi Wang <yiyi@michaels.com>
2021-02-25 14:36:09 -0800

    testing

--- eval.py
+++ eval.py
@@ -196,16 +196,10 @@ def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, ma
         # pdb.set_trace()
         # TESTING
         seg_masks = copy.deepcopy(masks)
-        # img_seg = img_gpu * seg_masks.prod(dim=0)
-        # img_seg = (img_seg * 255).byte().cpu().numpy()
-        # seg_path = seg_path[:-4] + "_" + str(random.randint(0, 1024)) + seg_path[-4:]
-        # cv2.imwrite(seg_path, img_seg)
-
-        # TODO: Save segmentations by thread ID.
-        # seg_masks[seg_masks==1] = 2
-        # seg_masks[seg_masks==0] = 1
-        # seg_masks[seg_masks==2] = 0
-        # TESTING
+        img_seg = img_gpu * seg_masks.prod(dim=0)
+        img_seg = (img_seg * 255).byte().cpu().numpy()
+        seg_path = seg_path[:-4] + "_" + str(random.randint(0, 1024)) + seg_path[-4:]
+        cv2.imwrite(seg_path, img_seg)
 
         # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
         colors = torch.cat([get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in range(num_dets_to_consider)], dim=0)
@@ -221,10 +215,10 @@ def prep_display(dets_out, img, h, w, undo_transform=True, class_color=False, ma
         if num_dets_to_consider > 1:
             inv_alph_cumul = inv_alph_masks[:(num_dets_to_consider-1)].cumprod(dim=0)
             # TEST segmentation per item
-            item_seg = seg_masks[1:].prod(dim=0)
-            item_seg = (item_seg * 255).byte().cpu().numpy()
-            seg_path = seg_path[:-4] + "_" + str(random.randint(0, 1024)) + seg_path[-4:]
-            cv2.imwrite(seg_path, item_seg)
+            # item_seg = seg_masks[1:].prod(dim=0)
+            # item_seg = (item_seg * 255).byte().cpu().numpy()
+            # seg_path = seg_path[:-4] + "_" + str(random.randint(0, 1024)) + seg_path[-4:]
+            # cv2.imwrite(seg_path, item_seg)
             # TEST
             masks_color_cumul = masks_color[1:] * inv_alph_cumul
             masks_color_summand += masks_color_cumul.sum(dim=0)
